39d34a6e2fdf9e068db91a03587ab56571105512
ZiadNasr <ziadnasr988@gmail.com>
2024-09-05 22:21:22 +0300

    Top bar in Maps

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-
-// TODO: NavBar fe el Map
+import {TopBar} from '../components/TopBar';
 
 const MapScreen = () => {
   const markers = [
     {
       title: 'Gate 1 Rehab Branch',
-      //   description: 'Marker Description',
       coordinates: {
         latitude: 30.0535448,
         longitude: 31.4977495,
@@ -16,7 +14,6 @@ const MapScreen = () => {
     },
     {
       title: 'Branch 2',
-      //   description: 'Marker Description',
       coordinates: {
         latitude: 30.0391081,
         longitude: 31.4837261,
@@ -38,24 +35,27 @@ const MapScreen = () => {
     },
   ];
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 30.0437544, // Example coordinates
-          longitude: 31.4888679,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        {markers.map((marker, index) => (
-          <Marker
-            key={index}
-            coordinate={marker.coordinates}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-      </MapView>
+    <View>
+      <TopBar />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 30.0437544, // Example coordinates
+            longitude: 31.4888679,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={marker.coordinates}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
+      </View>
     </View>
   );
 };
@@ -64,7 +64,8 @@ export default MapScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    height: '95%',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
